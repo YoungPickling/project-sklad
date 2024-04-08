f3ffae38f424c8b9709f6b0c4bfcf4745f76e79b
Maximas <maksimas.pavlenko@gmail.com>
2024-04-08 09:50:08 +0300

    fit: frontpage init

--- frontend/sklad/src/app/app.routes.ts
+++ frontend/sklad/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { WorkspaceComponent } from './workspace/workspace.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 export const routes: Routes = [
-  { path: "", component: WorkspaceComponent }, //workspace
+  { path: "", component: FrontpageComponent }, //workspace
+  { path: "workspace", component: WorkspaceComponent }, //workspace
 ];
---
+++ frontend/sklad/src/app/frontpage/frontpage.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './frontpage.component.html',
+  styleUrl: './frontpage.component.css'
+})
+export class FrontpageComponent {
+
+}
